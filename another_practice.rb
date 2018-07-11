c5b651ae03bbbb47761ab359c83f385361c8a8cf
avadavis5013 <adavis5013@gmail.com>
2018-07-11 17:40:43 +0000

    automatically backed up by learn

--- another_practice.rb
+++ another_practice.rb
@@ -1,13 +1,10 @@
-loop do
-  puts "Now watch me whip"
-  puts "Now watch me nae nae"
-  puts "Now watch me whip, whip"
-  puts "Now watch me nae nae"
-  puts "Oooh watch me, watch me"
-  puts "Oooh watch me, watch me"
-  puts "Oooh watch me, watch me"
+num_of_hotdogs_eaten = 0
+
+
+while num_of_hotdogs_eaten < 7
+  num_of_hotdogs_eaten += 1
+  puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)"
 end
 
-puts "Do the stanky leg"
-puts "Do the stanky leg"
-exit
+
+puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
