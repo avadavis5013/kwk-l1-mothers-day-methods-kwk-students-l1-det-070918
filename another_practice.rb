5c78eaa38cfc1e2526fff7ff57feb3a4e54d04d4
avadavis5013 <adavis5013@gmail.com>
2018-07-11 17:42:55 +0000

    automatically backed up by learn

--- another_practice.rb
+++ another_practice.rb
@@ -1,10 +1,5 @@
-num_of_hotdogs_eaten = 0
-
-
-while num_of_hotdogs_eaten < 7
-  num_of_hotdogs_eaten += 1
-  puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)"
+counter = 0
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1
 end
-
-
-puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
